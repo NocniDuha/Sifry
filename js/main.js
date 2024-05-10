ef8f7eeb3a31b1b78fbe237622b80b57b42a45ff
NocniDuha <116986484+NocniDuha@users.noreply.github.com>
2024-05-10 23:10:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-let slideIndex = 1;
+let hadanky = [
+  {otazka: "Otázka 1",
+   odpoved:"Odpověď 1"},
+  {otazka: "Otázka 2",
+   odpoved:"Odpověď 2"},
+  {otazka: "Otázka 3",
+   odpoved:"Odpověď 3"},
+  {otazka: "Otázka 4",
+   odpoved:"Odpověď 4"}
+];
 
+let otazka = document.getElementById("otazka");
+let odpoved = document.getElementById("odpoved");
+let pokus = document.getElementById("pokus");
+
+// Carousel
+let slideIndex = 1;
 showSlides(slideIndex);
-addEvent();
 
 // Next/previous controls
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -16,32 +29,24 @@ function currentSlide(n) {
 
 function showSlides(n) {
 
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let zk = document.getElementById('zkouska');
-  let od = document.getElementsByClassName('odpoved');
-
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-  }
+  if (n > hadanky.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = hadanky.length;}
+  otazka.innerHTML = hadanky[slideIndex-1].otazka;
+  odpoved.innerHMTL = hadanky[slideIndex-1].odpoved;
+}
 
 // Pridava event listener ke kazde karte
-function addEvent () {
-let karta = document.getElementsByClassName("karta");
-let odpoved = document.getElementsByClassName("odpoved");
-let zk = document.getElementById("zkouska");
-
-for (let i=0; i < karta.length; i++) {
-   karta[i].addEventListener('click', function () { //odpoved[i].style.display="block";
-     
-     if (odpoved[i].style.display == "none") {
-          odpoved[i].style.display = "block"} 
-     else {
-          odpoved[i].style.display="none"}
-         });
-       }
-}
+//function addEvent () {
+//let karta = document.getElementsByClassName("karta");
+//let zk = document.getElementById("zkouska");
+//
+//for (let i=0; i < hadanky.length; i++) {
+//   karta[i].addEventListener('click', function () { //odpoved[i].style.display="block";
+//     
+//     if (odpoved[i].style.display == "none") {
+//          odpoved[i].style.display = "block"} 
+//    else {
+//          odpoved[i].style.display="none"}
+//         });
+//       }
+//}
