4f047100914cd0a99c99e57ce5b91b33bdb58f16
NocniDuha <116986484+NocniDuha@users.noreply.github.com>
2024-05-11 02:40:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 let hadanky = [
-  {otazka: "Z CYKLU NEVŠEDNÍ ÚMRTÍ Ve 48 si podřezal hrdlo a probodl srdce. KDO je to?",
+  {dil:"1. díl",
+   tema:"Z cyklu nevšední úmrtí",
+    otazka: "Ve 48 si podřezal hrdlo a probodl srdce. KDO je to?",
    odpoved:"Vilém Mrštík"},
-  {otazka: "Jako malá dívka zbožňovala domečky pro panenky. Dokázala je zařizovat pidi dekoracemi celé hodiny. Když vyrostla, zbožňovala domy, kupovala je, zařizovala vkusným nábytkem a mnoha dekoracemi. Mohla si to dovolit. KDO je to?",
+  {dil:"2. díl",
+   tema:"",
+    otazka: "Jako malá dívka zbožňovala domečky pro panenky. Dokázala je zařizovat pidi dekoracemi celé hodiny. Když vyrostla, zbožňovala domy, kupovala je, zařizovala vkusným nábytkem a mnoha dekoracemi. Mohla si to dovolit. KDO je to?",
    odpoved:"Agatha Christie"},
-  {otazka: "POZNEJ LITERÁRNÍ DÍLO Schopnost tělesa odrážet světlo se pojí s nedostatkem životních potřeb profesionálních společnic",
+  {dil:"3. díl",
+   tema:"Poznej literární dílo",
+    otazka: "Schopnost tělesa odrážet světlo se pojí s nedostatkem životních potřeb profesionálních společnic",
    odpoved:"Lesk a bída kurtizán"}
 ];
 
+let dil = document.getElementById("dil");
+let tema = document.getElementById("tema");
 let otazka = document.getElementById("otazka");
 let odpoved = document.getElementById("odpoved");
 let karta = document.getElementById("karta");
@@ -31,8 +39,10 @@ function showSlides(n) {
 
   if (n > hadanky.length) {slideIndex = 1;}
   if (n < 1) {slideIndex = hadanky.length;}
+  dil.innerText = hadanky[slideIndex-1].dil;
+  tema.innerText = hadanky[slideIndex-1].tema;
   otazka.innerText = hadanky[slideIndex-1].otazka;
-  odpoved.style.display = "none";
+ // odpoved.style.display = "none";
   odpoved.innerText = hadanky[slideIndex-1].odpoved;
 }
 
